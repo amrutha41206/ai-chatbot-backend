d9c9b3e53a8f2b754dd27e39d65e9ddf44dda6fa
Amrutha Nagapuri <amruthanagapuri@Amruthas-MacBook-Air.local>
2026-05-20 09:47:51 +0530

    Added chatbot POST API

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class ChatRequest(BaseModel):
+    message: str
 
 @app.get("/")
 def home():
@@ -9,3 +13,12 @@ def home():
 @app.get("/health")
 def health():
     return {"status": "success"}
+
+@app.post("/chat")
+def chat(request: ChatRequest):
+    user_message = request.message
+
+    return {
+        "user_message": user_message,
+        "bot_reply": f"You said: {user_message}"
+    }
